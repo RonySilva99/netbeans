71df579acbbda03b832c790959f71a3455017afd
RonySilva99 <rony1115150@gmail.com>
2026-04-29 20:18:03 -0300

    Add files via upload

---
+++ exemplo_tela/src/main_Main.java
@@ -0,0 +1,23 @@
+
+import View.Inicio_GUI;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Laboratorio-Info
+ */
+public class main_Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         new Inicio_GUI().setVisible(true);0
+    }
+    
+}
